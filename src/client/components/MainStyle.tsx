e1e3f3db3a0632aa0e957b50e0f7439762b22f72
Lidcer <thelidcer@gmail.com>
2020-08-16 21:41:17 +0200

    make it center once again

--- src/client/components/MainStyle.tsx
+++ src/client/components/MainStyle.tsx
@@ -1,11 +1,16 @@
 import styled from 'styled-components';
 
 export const MainStyled = styled.div`
+  position: absolute;
   margin: auto;
-  width: 50%;
-  border: 3px solid green;
+  width: 50vh;
+  height: 50vh;
+  border: 3px inset green;
   padding: 10px;
-  text-align:center;
+  text-align: center;
+  left: 50%;
+  top: 50%;
+  transform: translateX(-25vh) translateY(-25vh);
 `;
 
 export const MainTitle = styled.h1`
