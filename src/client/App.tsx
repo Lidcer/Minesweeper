af6f4db65bbd4b80382b24deef842151f474929a
Lidcer <thelidcer@gmail.com>
2020-08-16 23:45:30 +0200

    Refactor code

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Main } from './components/Main';
+import { Main } from './components/Main/Main';
 import './App.scss';
 
 export class App extends React.Component {
---
+++ src/client/components/OfflineMode/Offline.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export class OfflineMode extends React.Component<{},{}> {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+  }
+  componentWillUnmount() {
+ 
+  }
+
+
+  render() {
+    return (<div>test</div>);
+  }
+}
